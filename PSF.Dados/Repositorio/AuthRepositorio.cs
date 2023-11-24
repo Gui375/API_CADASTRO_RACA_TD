f825419807321db48bdf3d0392a29b12c49680af
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-11-24 14:59:28 -0300

    Autenticação para o front-end

---
+++ PSF.Dados/Repositorio/AuthRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using PSF.Dados.EntityFramework;
+using PSF.Dados.Interface;
+using PSF.Dominio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSF.Dados.Repositorio
+{
+    public class AuthRepositorio : IAuthRepositorio
+    {
+        private readonly Contexto _applicationDbContext;
+        public AuthRepositorio(Contexto applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public Usuario BuscarUsuario(string usuario)
+        {
+            return _applicationDbContext.Usuario.Where(u => u.Nome == usuario).FirstOrDefault();
+        }
+
+        public async Task<Usuario> Cadastrar(Usuario usuario)
+        {
+            _applicationDbContext.Usuario.Add(usuario);
+            await _applicationDbContext.SaveChangesAsync();
+            return usuario;
+        }
+    }
+}
---
+++ PSF.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
+using PSF.Dados.Interface;
+using PSF.Dominio.Entities;
+using PSF.WebApp.Helpers;
+
+namespace PSF.WebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepositorio _authRepositorio;
+        public AuthController(IAuthRepositorio authRepositorio)
+        {
+            _authRepositorio = authRepositorio;
+        }
+
+        [HttpPost]
+        [Route("Cadastrar")]
+        public async Task<ActionResult<Usuario>> Cadastrar(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Dados inválidos");
+            usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
+            var result = await _authRepositorio.Cadastrar(usuario);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<ActionResult<dynamic>> Login([FromBody] string usuario, string senha)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Dados inválidos");
+
+            var user = _authRepositorio.BuscarUsuario(usuario);
+            if (user == null)
+            {
+                return BadRequest("Usuário não encontrado");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(user.Senha, senha))
+            {
+                return BadRequest("Credenciais Inválidas");
+            }
+
+            string token = TokenService.GenerateToken(user);
+
+            return Ok(token);
+        }
+
+    }
+}
