bf1c230410a45f7702b10420a408eb83ec053828
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-11-27 14:18:59 -0300

    Raça e porte refatorados

---
+++ PSF.Dados/Repositorio/PorteRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using PSF.Dados.EntityFramework;
+using PSF.Dados.Interface;
+using PSF.Dominio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSF.Dados.Repositorio
+{
+    public class PorteRepositorio : Repositorio<Porte>, IPorteRepositorio
+    {
+        public PorteRepositorio(Contexto context) : base(context)
+        {
+
+        }
+      
+    }
+}
---
+++ PSF.Dados/Repositorio/RacaRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using PSF.Dados.EntityFramework;
+using PSF.Dados.Interface;
+using PSF.Dominio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSF.Dados.Repositorio
+{
+    public class RacaRepositorio : Repositorio<Raca>, IRacaRepositorio
+    {
+        public RacaRepositorio(Contexto context) : base(context)
+        {
+
+        }
+
+      
+    }
+}
